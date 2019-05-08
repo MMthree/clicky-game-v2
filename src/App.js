922918e9d46365c2aea4c55c7463aa0ff3f33462
Mario Miramontes <mmisthree@gmail.com>
2019-05-08 16:59:53 -0700

    added images for every mode. onClick shuffle images working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Easy from "./easy.json";
 import Medium from "./medium.json";
-
+import Hard from "./hard.json"
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Card from "./components/Card";
@@ -10,8 +10,8 @@ import Card from "./components/Card";
 
 class App extends Component {
   state={
-    currentScore: 4,
-    highScore: 3,
+    currentScore: 0,
+    highScore: 0,
     message: "",
     difficulty: "",
     gameData: "",
@@ -24,11 +24,35 @@ class App extends Component {
     if (mode === "Medium") {
       this.setState({ gameData: Medium })
     }
-    // if (mode === "Hard") {
-    //   this.setState({ gameData: Hard })
-    // }
+    if (mode === "Hard") {
+      this.setState({ gameData: Hard })
+    }
 
     this.setState({ difficulty: mode});
+  };
+
+  shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return this.setState({ gameData: a });
+    
+  }
+
+  game = id => {
+    console.log(id)
+    const find = this.state.gameData.find(pic => (pic.id === id));
+
+    if (find === undefined) {
+      this.setState({
+        messsage: "Game Over",
+        currentScore: 0,
+        highScore: (this.state.currentScore > this.state.highScore) ? this.state.currentScore : this.state.highScore,
+      })
+    } 
+
+    this.shuffle(this.state.gameData);
   };
 
 
@@ -55,6 +79,9 @@ class App extends Component {
           {this.state.gameData && this.state.gameData.map(data => { 
             return(
               <Card 
+              key={data.id}
+              game={this.game}
+              id={data.id}
               image={data.image}
               />
             )
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = props => (
     <div className="mx-auto">
-        <div className="col-6 col-sm-6 col-md-3 my-3">
-            <img className="rounded" alt="d" src={props.image}></img>
+        <div key={props.id} onClick={() => props.game(props.id)} className="col-6 col-sm-6 col-md-3 my-3">
+            <img className="rounded" alt={props.id} src={props.image}></img>
         </div>
     </div>
 );
