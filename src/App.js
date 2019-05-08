226d6b7e2c7dad164312d7e5e91f27c6b44f3683
Mario Miramontes <mmisthree@gmail.com>
2019-05-07 18:26:49 -0700

    create react app init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button className="btn btn-primary">test</button>
+    </div>
+  );
+}
+
+export default App;
