6b494ad31e9cb2ed2746cda3cbc8d304e7c2ce68
Mario Miramontes <mmisthree@gmail.com>
2019-05-09 21:12:01 -0700

    Updates

---
+++ precache-manifest.118aa233e879a74b2de5c3f1134b326a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04eb7e932f026371798c30e82b9dfa0b",
+    "url": "/clicky-game-v2/index.html"
+  },
+  {
+    "revision": "bd247cf65c2bd055805c",
+    "url": "/clicky-game-v2/static/css/main.049fb739.chunk.css"
+  },
+  {
+    "revision": "daa69e343bedf3fa9eb8",
+    "url": "/clicky-game-v2/static/js/2.48c8e16c.chunk.js"
+  },
+  {
+    "revision": "bd247cf65c2bd055805c",
+    "url": "/clicky-game-v2/static/js/main.ec721e44.chunk.js"
+  },
+  {
+    "revision": "f89e30bd491a7d24d63c",
+    "url": "/clicky-game-v2/static/js/runtime~main.27d8473f.js"
+  }
+]);
